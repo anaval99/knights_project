e1e5346fe8e60a39f6e9fe3a576ca6f885adebe9
unknown <arnold.naval99@proton.me>
2025-07-11 12:07:17 -0600

    implement books and bars

--- Assets/__prefabs/PlayerRig/CharDataCenter.cs
+++ Assets/__prefabs/PlayerRig/CharDataCenter.cs
@@ -9,6 +9,8 @@ public class CharDataCenter : MonoBehaviour
     #region Events
     public BehaviorSubject<CharEquipment> CharEquipmentObs = new(null);
     public BehaviorSubject<CharInventory> CharInventoryObs = new(null);
+    public BehaviorSubject<CharSkillBooks> CharSkillBooksObs = new(null);
+    public BehaviorSubject<CharSkillBar> CharSkillBarObs = new(null);
     #endregion Events
     [SerializeField]
     EquipmentRenderer equipmentRenderer;
@@ -20,13 +22,18 @@ public class CharDataCenter : MonoBehaviour
         // get the character data from FirebaseService.Instance
         var charAvatar = await FirebaseService.Instance.GetSingle<CharAvatar>(FirebasePaths.Avatars, firebaseUid);
         var charInventory = await this.GetInventoryAsync(firebaseUid);
+        var charSkillBooks = await this.GetSkillBooksAsync(firebaseUid);
+        var charSkillBar = await this.GetSkillBarAsync(firebaseUid);
         var charEquipment = await this.GetEquipmentAsync(firebaseUid, charInventory);
         // set avatar
         this.bodyPartRenderer.SetBodyPart(charAvatar);
         // set equipment
         this.equipmentRenderer.SetEquipment(charEquipment);
+        // emit the loaded data
         this.CharEquipmentObs.OnNext(charEquipment);
         this.CharInventoryObs.OnNext(charInventory);
+        this.CharSkillBooksObs.OnNext(charSkillBooks);
+        this.CharSkillBarObs.OnNext(charSkillBar);
     }
 
     public async Task SaveEquipment(CharEquipment equipment)
@@ -61,5 +68,27 @@ public class CharDataCenter : MonoBehaviour
             await FirebaseService.Instance.SaveSingle(FirebasePaths.Equipments, charEquipment);
         }
         return charEquipment;
+    }
+
+    public async Task<CharSkillBooks> GetSkillBooksAsync(string firebaseUid)
+    {
+        var charSkillBooks = await FirebaseService.Instance.GetSingle<CharSkillBooks>(FirebasePaths.SkillBooks, firebaseUid);
+        if (charSkillBooks == null)
+        {
+            charSkillBooks = new CharSkillBooks() { UserId = firebaseUid };
+            await FirebaseService.Instance.SaveSingle(FirebasePaths.SkillBooks, charSkillBooks);
+        }
+        return charSkillBooks;
+    }
+
+    public async Task<CharSkillBar> GetSkillBarAsync(string firebaseUid)
+    {
+        var charSkillBar = await FirebaseService.Instance.GetSingle<CharSkillBar>(FirebasePaths.SkillBars, firebaseUid);
+        if (charSkillBar == null)
+        {
+            charSkillBar = new CharSkillBar() { UserId = firebaseUid };
+            await FirebaseService.Instance.SaveSingle(FirebasePaths.SkillBars, charSkillBar);
+        }
+        return charSkillBar;
     }
 }
--- Assets/__sharedScripts/Models/CharSkillBooks.cs
+++ Assets/__sharedScripts/Models/CharSkillBooks.cs
@@ -8,7 +8,7 @@ public class CharSkillBooks: IFirebaseDoc
   [FirestoreProperty]
   public List<SkillBook> SkillBooks { get; set; } = new List<SkillBook>()
   {
-    new() { SkillBookSOId = "normal_sword", Quantity = 1 },
+    new() { SkillBookSOId = "beginner_slash", Quantity = 1 },
   };
   [FirestoreProperty]
   public string UserId { get; set; } = string.Empty;
