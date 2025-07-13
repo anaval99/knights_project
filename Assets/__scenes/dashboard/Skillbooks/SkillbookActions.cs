7cfaf2a62766814f4588ffa95db71bdd5d8e6690
unknown <arnold.naval99@proton.me>
2025-07-12 22:37:32 -0600

    Refactored SkillbookUI for improved clarity and IL2CPP compatibility\n\n- Replaced runtime lambda functions with explicit methods to ensure compatibility with IL2CPP on iOS.\n- Enhanced code readability and maintainability by simplifying logic in Start and Render methods.\n- Ensured all types and methods used in lambdas are explicitly referenced to prevent stripping during AOT compilation.\n- Set up SkillbookActions and SkillbookUI editor references for better integration and functionality.

--- Assets/__scenes/dashboard/Skillbooks/SkillbookActions.cs
+++ Assets/__scenes/dashboard/Skillbooks/SkillbookActions.cs
@@ -1,11 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using R3;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillbookActions : MonoBehaviour
 {
+    [SerializeField]
+    CharDataCenter charDataCenter;
+    [SerializeField]
+    List<Button> equipSkillButtons;
+
+    public BehaviorSubject<SkillBook> SelectedSkillBookObs = new(null);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        var equipIndexObs = Observable.Merge(this.equipSkillButtons.Select(button => button.OnClickAsObservable().Select(_ => this.equipSkillButtons.IndexOf(button))));
+        var skillBarObs = this.charDataCenter.CharSkillBarObs.Where(x => x != null);
+        equipIndexObs.CombineLatest(skillBarObs, (equipIndex, skillBar) =>
+        {
+            return (equipIndex, skillBar);
+        })
+        .Where(_ => this.SelectedSkillBookObs.Value != null)
+        .Subscribe(index_skillbar =>
+        {
+            var (equipIndex, skillBar) = index_skillbar;
+            Debug.Log($"Equip index: {equipIndex}, SkillBar: {this.SelectedSkillBookObs.Value.SkillBookSOId}");
+        })
+        .AddTo(this);
+    }
+
+    void OnEnable()
+    {
+        this.SelectedSkillBookObs.OnNext(null);
     }
 
     // Update is called once per frame
--- Assets/__scenes/dashboard/Skillbooks/SkillbookUI.cs
+++ Assets/__scenes/dashboard/Skillbooks/SkillbookUI.cs
@@ -1,4 +1,6 @@
+using R3;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillbookUI : MonoBehaviour
 {
@@ -10,14 +12,40 @@ public class SkillbookUI : MonoBehaviour
     UnityEngine.UI.Image backgroundImage;
     [SerializeField]
     GameObject FocusedSymbol;
+    [SerializeField]
+    SkillbookActions skillbookActions;
+    [SerializeField]
+    Button skillUIButton;
+
+    private SkillBook currentSkillBook;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        this.skillUIButton.OnClickAsObservable()
+            .Where(_ => this.currentSkillBook != null)
+            .Subscribe(_ =>
+            {
+                this.skillbookActions.SelectedSkillBookObs.OnNext(this.currentSkillBook);
+            })
+            .AddTo(this);
 
+        this.skillbookActions.SelectedSkillBookObs
+            .Subscribe(selectedSkillBook =>
+            {
+                bool focused = false;
+                if (selectedSkillBook != null && this.currentSkillBook != null)
+                {
+                    focused = selectedSkillBook.SkillBookSOId == this.currentSkillBook.SkillBookSOId;
+                    this.FocusedSymbol.SetActive(focused);
+                }
+            })
+            .AddTo(this);
     }
 
     public void Render(SkillBook skillBook)
     {
+        this.currentSkillBook = skillBook;
+        this.FocusedSymbol.SetActive(false);
         this.itemImage.gameObject.SetActive(false);
         this.backgroundImage.color = Color.black;
         if (skillBook == null)
@@ -31,6 +59,10 @@ public class SkillbookUI : MonoBehaviour
             itemImage.sprite = skillBookSO.SkillIcon;
             itemImage.gameObject.SetActive(true);
             this.backgroundImage.color = new Color(0.2f, 0.2f, 0.2f, 1f);
+            if (this.skillbookActions.SelectedSkillBookObs.Value != null)
+            {
+                this.FocusedSymbol.SetActive(this.skillbookActions.SelectedSkillBookObs.Value.SkillBookSOId == skillBook.SkillBookSOId);
+            }
         }
     }
 }
