43f1e25d44660492840bfee35b5d649853ebae41
unknown <arnold.naval99@proton.me>
2025-07-20 21:49:37 -0600

    fix issue where update was used instead of start

--- Assets/__sharedScripts/StateMachine/RxStateMachine.cs
+++ Assets/__sharedScripts/StateMachine/RxStateMachine.cs
@@ -7,12 +7,6 @@ public class RxStateMachine : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         this.CurrentStateObs.Where(state => state != null)
             .Select(state => state.Play())
             .Switch()
