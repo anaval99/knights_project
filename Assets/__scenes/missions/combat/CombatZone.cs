afda54cac7aeb5dc2f37945852d101beec82eb96
unknown <arnold.naval99@proton.me>
2025-07-21 22:24:30 -0600

    enemy detection!

--- Assets/__scenes/missions/combat/CombatZone.cs
+++ Assets/__scenes/missions/combat/CombatZone.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Animancer.FSM;
 using UnityEngine;
 
@@ -5,10 +7,13 @@ public class CombatZone : MonoBehaviour
 {
     bool hasTriggered = false;
     private CombatController combatController;
+    private Collider selfCollider;
     void Start()
     {
         combatController = GameObject.FindGameObjectWithTag("CombatController").GetComponent<CombatController>();
+        selfCollider = GetComponent<Collider>();
     }
+
     void OnTriggerEnter(Collider other)
     {
         string otherLayer = LayerMask.LayerToName(other.gameObject.layer);
@@ -21,6 +26,35 @@ public class CombatZone : MonoBehaviour
                 Phase = CombatPhase.BattleStart
             };
             combatController.SetCombatState(state);
+            var enemies = GetCombatParticipants();
+            Debug.Log("CombatZone found " + enemies.Count + " combat participants: " + string.Join(", ", enemies.Select(e => e.name)));
+
         }
     }
+
+    List<CombatParticipant> GetCombatParticipants()
+    {
+        // get all the <CombatParticipant> within the combat zone
+        List<CombatParticipant> participants = new List<CombatParticipant>();
+        Collider[] colliders = Physics.OverlapBox(this.transform.position, selfCollider.bounds.extents, Quaternion.identity);
+        foreach (Collider collider in colliders)
+        {
+            CombatParticipant participant = collider.GetComponent<CombatParticipant>();
+            if (participant != null)
+            {
+                participants.Add(participant);
+            }
+        }
+        return participants;
+    }
+
+    void OnDrawGizmos()
+    {   
+        if (selfCollider == null)
+        {
+            selfCollider = GetComponent<Collider>();
+        }
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(this.transform.position, selfCollider.bounds.size);
+    }    
 }
